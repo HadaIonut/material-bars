88f4f0a0d58ecc58c1f80dc0bc98211132c8e3fc
Marian <33843056+bmarian@users.noreply.github.com>
2020-12-27 20:56:25 +0200

    Added node-vibrant

--- scripts/macro-temp.js
+++ scripts/macro-temp.js
@@ -1,51 +1,17 @@
-function getBgAvgColor() {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(getImgAvgColor(img));
-        img.onerror = reject;
-        img.src = game?.scenes?.viewed?.img;
-    });
-}
+const v = new Vibrant(game?.scenes?.viewed?.img);
+v.getPalette((err, palette) => {
+    const {Vibrant, Muted, LightVibrant, LightMuted, DarkVibrant, DarkMuted} = palette;
+    console.log(Vibrant, Muted, LightVibrant, LightMuted, DarkVibrant, DarkMuted);
 
-function getImgAvgColor(img) {
-    const defaultRGB = {r: 0, g:0, b: 0}; // ?? for non-supporting envs 
-    const RGB = {...defaultRGB};
-    const tempCanvas = document.createElement('canvas');
-    const context = typeof tempCanvas.getContext !== 'undefined' && tempCanvas.getContext('2d');
-
-    if (!context) return defaultRGB;
-
-    const height = tempCanvas.height = img.naturalHeight || img.offsetHeight || img.height;
-    const width = tempCanvas.width = img.naturalWidth || img.offsetWidth || img.width;
-
-    // ??
-    context.drawImage(img, 0, 0);
-
-    let data = null;
-    try {
-        data = context.getImageData(0, 0, width, height)?.data;
-    } catch (e) {
-        // security error, img on diff domain
-        console.error(e);
-        return defaultRGB;
-    }
-
-    const blockSize = 5; // only visit every 5 pixels
-    const length = data.length;
-    let count = 0;
-
-    for (let i = -4 + blockSize * 4; i < length; i += blockSize * 4, ++count) {
-        RGB.r += data[i];
-        RGB.g += data[i + 1];
-        RGB.b += data[i + 2];
-    }
-
-
-    //In case anything breaks here bother @bmarian to fix, it's his shit not mine
-    return Object.entries(RGB).reduce((newRGB, [space, color]) => {
-        newRGB[space] = Math.floor(color / count);
-        return newRGB;
-    }, {});
-};
-
-console.log(await getBgAvgColor());
+    const content = `
+        <ul style="color: white">
+            <li><p style="background-color: rgb(${Vibrant._rgb[0]},${Vibrant._rgb[1]},${Vibrant._rgb[2]})">Vibrant</p><li>
+            <li><p style="background-color: rgb(${Muted._rgb[0]},${Muted._rgb[1]},${Muted._rgb[2]})">Muted</p><li>
+            <li><p style="background-color: rgb(${LightVibrant._rgb[0]},${LightVibrant._rgb[1]},${LightVibrant._rgb[2]})">LightVibrant</p><li>
+            <li><p style="background-color: rgb(${LightMuted._rgb[0]},${LightMuted._rgb[1]},${LightMuted._rgb[2]})">LightMuted</p><li>
+            <li><p style="background-color: rgb(${DarkVibrant._rgb[0]},${DarkVibrant._rgb[1]},${DarkVibrant._rgb[2]})">DarkVibrant</p><li>
+            <li><p style="background-color: rgb(${DarkMuted._rgb[0]},${DarkMuted._rgb[1]},${DarkMuted._rgb[2]})">DarkMuted</p><li>
+        </ul>
+    `;
+    ChatMessage.create({content});
+});
