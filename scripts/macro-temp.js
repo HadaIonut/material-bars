18b6acb10241a359a5649ca1b89cfbbdb32bf821
Ionut Hada <hadaionut@gmail.com>
2020-12-27 20:01:15 +0200

    Marian's rework of the macro

--- scripts/macro-temp.js
+++ scripts/macro-temp.js
@@ -1,59 +1,51 @@
-console.log(await printImgAvgColor());
-
-function printImgAvgColor() {
+function getBgAvgColor() {
     return new Promise((resolve, reject) => {
-        let img = new Image();
-        img.onload = function () {
-            resolve(getAverageRGB(img));
-        };
+        const img = new Image();
+        img.onload = () => resolve(getImgAvgColor(img));
         img.onerror = reject;
-        img.src = game.scenes.viewed.img;
+        img.src = game?.scenes?.viewed?.img;
     });
 }
 
-function getAverageRGB(imgEl) {
+function getImgAvgColor(img) {
+    const defaultRGB = {r: 0, g:0, b: 0}; // ?? for non-supporting envs 
+    const RGB = {...defaultRGB};
+    const tempCanvas = document.createElement('canvas');
+    const context = typeof tempCanvas.getContext !== 'undefined' && tempCanvas.getContext('2d');
 
-    var blockSize = 5, // only visit every 5 pixels
-        defaultRGB = {r: 0, g: 0, b: 0}, // for non-supporting envs
-        canvas = document.createElement('canvas'),
-        context = canvas.getContext && canvas.getContext('2d'),
-        data, width, height,
-        i = -4,
-        length,
-        rgb = {r: 0, g: 0, b: 0},
-        count = 0;
+    if (!context) return defaultRGB;
 
-    if (!context) {
-        return defaultRGB;
-    }
+    const height = tempCanvas.height = img.naturalHeight || img.offsetHeight || img.height;
+    const width = tempCanvas.width = img.naturalWidth || img.offsetWidth || img.width;
 
-    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+    // ??
+    context.drawImage(img, 0, 0);
 
-    context.drawImage(imgEl, 0, 0);
-
+    let data = null;
     try {
-        data = context.getImageData(0, 0, width, height);
+        data = context.getImageData(0, 0, width, height)?.data;
     } catch (e) {
-        /* security error, img on diff domain */
+        // security error, img on diff domain
         console.error(e);
         return defaultRGB;
     }
 
-    length = data.data.length;
+    const blockSize = 5; // only visit every 5 pixels
+    const length = data.length;
+    let count = 0;
 
-    while ((i += blockSize * 4) < length) {
-        ++count;
-        rgb.r += data.data[i];
-        rgb.g += data.data[i + 1];
-        rgb.b += data.data[i + 2];
+    for (let i = -4 + blockSize * 4; i < length; i += blockSize * 4, ++count) {
+        RGB.r += data[i];
+        RGB.g += data[i + 1];
+        RGB.b += data[i + 2];
     }
 
-    // ~~ used to floor values
-    rgb.r = ~~(rgb.r / count);
-    rgb.g = ~~(rgb.g / count);
-    rgb.b = ~~(rgb.b / count);
 
-    return rgb;
+    //In case anything breaks here bother @bmarian to fix, it's his shit not mine
+    return Object.entries(RGB).reduce((newRGB, [space, color]) => {
+        newRGB[space] = Math.floor(color / count);
+        return newRGB;
+    }, {});
+};
 
-}
+console.log(await getBgAvgColor());
